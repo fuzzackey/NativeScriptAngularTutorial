a3af5a2643de60c60ce6a8aa6eef97fd28eb56b1
zaki <zaki.zahri@canang.com.my>
2018-04-19 14:19:33 +0800

    chapter2: Add a logo

--- Groceries/platforms/ios/Groceries/app/app.component.ts
+++ Groceries/platforms/ios/Groceries/app/app.component.ts
@@ -1,9 +1,10 @@
-import { Component } from "@angular/core";
+import {Component} from "@angular/core";
 
 @Component({
     selector: "my-app",
     template: `
         <StackLayout>
+            <Image src="res://logo_login" stretch="none" horizontalAlignment="center"></Image>
             <TextField hint="Email Address" keyboardType="email"
                        autocorrect="false" autocapitalizationType="none"></TextField>
             <TextField hint="Password" secure="true"></TextField>
@@ -14,4 +15,5 @@ import { Component } from "@angular/core";
     `,
     styleUrls: ["pages/login/login-common.css", "pages/login/login.css"]
 })
-export class AppComponent {}
+export class AppComponent {
+}
