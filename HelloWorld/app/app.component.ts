68dd91e8fc352da67e91fb98662184912d49dd10
zaki <zaki.zahri@canang.com.my>
2018-04-19 12:57:33 +0800

    chapter1: Console logging in NativeScript

--- HelloWorld/app/app.component.ts
+++ HelloWorld/app/app.component.ts
@@ -5,16 +5,13 @@ import { Component } from "@angular/core";
     template: `
         <ActionBar title="My Apple" class="action-bar"></ActionBar>
         <Image src="~/images/apple.jpg"></Image>
-    `,
-    styles: [`
-    @keyframes spin {
-      from { transform: rotate(0); } to { transform: rotate(360); }
-    }
-    Image {
-      animation-name: spin; animation-duration: 3s;
-      animation-iteration-count: infinite;
-      animation-timing-function: linear;
-    }
-  `]
+    `
 })
-export class AppComponent {}
+export class AppComponent {
+    constructor() {
+        console.dir({
+            type: "Apple",
+            color: "Red"
+        });
+    }
+}
