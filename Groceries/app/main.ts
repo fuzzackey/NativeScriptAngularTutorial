f30dabef558d92fbcc2e1f24277fc86c86fe9f48
zaki <zaki.zahri@canang.com.my>
2018-04-19 17:39:53 +0800

    chapter6: Making translucent status bars

--- Groceries/app/main.ts
+++ Groceries/app/main.ts
@@ -1,5 +1,7 @@
-import { platformNativeScriptDynamic } from "nativescript-angular/platform";
+import {setStatusBarColors} from "./utils/status-bar-util";
+import {platformNativeScriptDynamic} from "nativescript-angular/platform";
 
-import { AppModule } from "./app.module";
+import {AppModule} from "./app.module";
 
+setStatusBarColors();
 platformNativeScriptDynamic().bootstrapModule(AppModule);
