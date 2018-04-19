6b592b54a390ddb9ddf277cd26a21ee943e90b08
zaki <zaki.zahri@canang.com.my>
2018-04-19 13:14:24 +0800

    chapter2: Add UI elements to app.component.ts

---
+++ Groceries/platforms/ios/build/Groceries.build/Debug-iphonesimulator/Groceries.build/Objects-normal/x86_64/main.d
@@ -0,0 +1,11 @@
+dependencies: \
+  /Users/terang/WebstormProjects/NativeScriptAngularTutorial/Groceries/platforms/ios/Groceries/Groceries-Prefix.pch \
+  /Users/terang/WebstormProjects/NativeScriptAngularTutorial/Groceries/platforms/ios/internal/main.m \
+  /Users/terang/WebstormProjects/NativeScriptAngularTutorial/Groceries/platforms/ios/internal/NativeScript.framework/Headers/NativeScript.h \
+  /Users/terang/WebstormProjects/NativeScriptAngularTutorial/Groceries/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntimeInstrumentation.h \
+  /Users/terang/WebstormProjects/NativeScriptAngularTutorial/Groceries/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime.h \
+  /Users/terang/WebstormProjects/NativeScriptAngularTutorial/Groceries/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime+Diagnostics.h \
+  /Users/terang/WebstormProjects/NativeScriptAngularTutorial/Groceries/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime+Inspector.h \
+  /Users/terang/WebstormProjects/NativeScriptAngularTutorial/Groceries/platforms/ios/internal/TNSExceptionHandler.h \
+  /Users/terang/WebstormProjects/NativeScriptAngularTutorial/Groceries/platforms/ios/internal/TNSDebugging.h \
+  /Users/terang/WebstormProjects/NativeScriptAngularTutorial/Groceries/platforms/ios/internal/TKLiveSync/include/TKLiveSync.h
